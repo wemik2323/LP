7d5b86a72b6aadfbda27946401a0722e3fc2788c
wemik2323 <wemik232@yandex.ru>
2024-09-26 14:28:54 +0300

    Praks TA changed to var 1

--- TAandFA/lab3/task.py
+++ TAandFA/lab3/task.py
@@ -6,70 +6,36 @@ class VendingMachine:
         if self.state == 0:
             if coin == 1:
                 self.state = 1
+                return -1
             elif coin == 2:
-                self.state = 2
+                return 0
             elif coin == 5:
-                self.state = 5
-        elif self.state == 1:
-            if coin == 1:
-                self.state = 2
-            elif coin == 2:
-                self.state = 3
-            elif coin == 5:
-                self.state = 6
-        elif self.state == 2:
-            if coin == 1:
-                self.state = 3
-            elif coin == 2:
-                self.state = 4
-            elif coin == 5:
-                self.state = 7
-        elif self.state == 3:
-            if coin == 1:
-                self.state = 4
-            elif coin == 2:
-                self.state = 5
-            elif coin == 5:
-                self.state = 8
-        elif self.state == 4:
-            if coin == 1:
-                self.state = 5
-            elif coin == 2:
-                self.state = 6
-            elif coin == 5:
-                self.state = 9
-        elif self.state == 5:
-            self.state = 0
-        elif self.state == 6:
-            self.state = 0
-        elif self.state == 7:
-            self.state = 0
-        elif self.state == 8:
-            self.state = 0
-        elif self.state == 9:
-            self.state = 0
+                return 3
+            elif coin == 10:
+                return 8
 
-    def output_state(self):
-        if self.state == 0:
-            print("Сумма: 0 рублей, ничего не делаем")
         elif self.state == 1:
-            print("Сумма: 1 рубль, ничего не делаем")
-        elif self.state == 2:
-            print("Сумма: 2 рубля, ничего не делаем")
-        elif self.state == 3:
-            print("Сумма: 3 рубля, ничего не делаем")
-        elif self.state == 4:
-            print("Сумма: 4 рубля, ничего не делаем")
-        elif self.state == 5:
-            print("Сумма: 5 рублей, выдаём товар")
-        elif self.state == 6:
-            print("Сумма: 6 рублей, выдаём товар и сдачу 1 рубль")
-        elif self.state == 7:
-            print("Сумма: 7 рублей, выдаём товар и сдачу 2 рубля")
-        elif self.state == 8:
-            print("Сумма: 8 рублей, выдаём товар и сдачу 3 рубля")
-        elif self.state == 9:
-            print("Сумма: 9 рублей, выдаём товар и сдачу 4 рубля")
+            if coin == 1:
+                self.state = 0
+                return 0
+            elif coin == 2:
+                self.state = 0
+                return 1
+            elif coin == 5:
+                self.state = 0
+                return 4
+            elif coin == 10:
+                self.state = 0
+                return 9
+
+    def output_state(self, change):
+        if self.state == 0:
+            if change >= 0:
+                print(f"Сумма: 0 рублей. Выдаем товар. Сдача: {change}")
+            elif change < 0:
+                print("Сумма: 0 рублей. AFK.")
+        elif self.state == 1:
+            print("Сумма: 1 рубль. AFK.")
 
 
 if __name__ == "__main__":
@@ -77,12 +43,12 @@ if __name__ == "__main__":
 
     while True:
         try:
-            coin = int(input("Вставьте монету (1, 2, 5): "))
-            if coin not in [1, 2, 5]:
+            coin = int(input("Вставьте монету (1, 2, 5, 10): "))
+            if coin not in [1, 2, 5, 10]:
                 print("Неверная монета. Попробуйте снова.")
                 continue
-            vm.insert_coin(coin)
-            vm.output_state()
+            change = vm.insert_coin(coin)
+            vm.output_state(change)
         except ValueError:
             print("Пожалуйста, введите числовое значение.")
 
